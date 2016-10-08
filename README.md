2bc32b335f1a014ccf36e53ddf7bc8449c381f03
Xuanyu Liu <sunflowerxuanyu@gmail.com>
2016-10-08 15:43:59 -0400

    adding real time chat -v1

---
+++ README.md
@@ -0,0 +1,15 @@
+# Real Time Chat
+
+## Notice: this project based on [Krasimir Tsonev](https://code.tutsplus.com/tutorials/real-time-chat-with-nodejs-socketio-and-expressjs--net-31708)'s tutorial
+
+1. Tech:
+
+    ExpressJS - this will manage the server and the response to the user
+
+    [Jade](http://jadelang.net/) - template engine
+
+    Socket.io - allows for real time communication between the front-end and back-end
+
+2. What to do next:
+
+  - Apply Bootstrap
---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Serena on 10/8/16.
+ */
+
+
+var express = require("express");
+var app = express();
+var port = 3700;
+
+/*This code informs Express where your template files are, and which template engine to use.
+ It all specifies the function that will process the template's code. Once everything is setup,
+ we can use the .render method of the response object, and simply send our Jade code to the user.*/
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
+
+// app.get("/", function(req, res){
+//     res.send("It works!");
+// });
+
+/*Because we will use an external JavaScript file that will hold the front-end logic,
+we need to inform ExpressJS where to look for such resources.*/
+app.use(express.static(__dirname + '/public'));
+
+/*passed the ExpressJS server to Socket.io*/
+var io = require('socket.io').listen(app.listen(port));
+// app.listen(port);
+console.log("Listening on port " + port);
+
+// Socket.io connection handler
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+})
+
